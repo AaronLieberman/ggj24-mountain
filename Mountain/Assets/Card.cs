3fcaf6ab41418d2a5715d64db031f9bb902bad75
Aaron Lieberman <aaronlcode@gmail.com>
2024-01-27 00:19:27 -0800

    Some more early card and tile definition and spawning, plus first cards

--- Mountain/Assets/Card.cs
+++ Mountain/Assets/Card.cs
@@ -1,18 +1,48 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+
+[Serializable]
+public class TileSpawnDesc
+{
+    public Tile Tile;
+    public float Weight = 1;
+}
 
 public class Card : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public List<TileSpawnDesc> TileSpawn;
+
+    Unity.Mathematics.Random _random = new();
+
+    public void SpawnAtLocation(Tile tile)
     {
-        
+        Utilities.DestroyAllChildren(tile.transform);
+
+        var chosenTile = ChooseTileToSpawn();
+        if (chosenTile == null)
+            return;
+
+        Instantiate(chosenTile, tile.transform);
     }
 
-    // Update is called once per frame
-    void Update()
+    Tile ChooseTileToSpawn()
     {
-        
+        var totalWeight = TileSpawn.Sum(a => a.Weight);
+        if (totalWeight <= 0)
+            return null;
+
+        var target = _random.NextFloat(totalWeight);
+
+        float weightSoFar = 0;
+        foreach (var spawn in TileSpawn)
+        {
+            weightSoFar += spawn.Weight;
+            if (target < weightSoFar)
+                return spawn.Tile;
+        }
+
+        return null;
     }
 }
--- Mountain/Assets/GameManager.cs
+++ Mountain/Assets/GameManager.cs
@@ -12,18 +12,13 @@ public class GameManager : MonoBehaviour
     public Deck Deck { get; private set; }
     public Hand Hand { get; private set; }
 
-    public T GetRootComponent<T>() where T : Component
-    {
-        return SceneManager.GetActiveScene().GetRootGameObjects().Select(a => a.GetComponent<T>()).Single(a => a != null);
-    }
-
     void Awake()
     {
-        Map = GetRootComponent<TileGridLayout>();
+        Map = Utilities.GetRootComponent<TileGridLayout>();
         Map.name = $"Map";
 
-        Deck = GetRootComponent<Deck>();
-        Hand = GetRootComponent<Hand>();
+        Deck = Utilities.GetRootComponent<Deck>();
+        Hand = Utilities.GetRootComponent<Hand>();
     }
 
     void Start()
@@ -39,12 +34,12 @@ public class GameManager : MonoBehaviour
 
         Hand.DrawTillFull();
 
-        var worker = Instantiate(WorkerPrefab, Map.transform);
+        // var worker = Instantiate(WorkerPrefab, Map.transform);
 
-        worker.AddWaypoint(new Vector2Int(1, 5));
-        worker.AddWaypoint(new Vector2Int(10, 6));
-        worker.AddWaypoint(new Vector2Int(3, 2));
-        worker.AddWaypoint(new Vector2Int(12, 14));
+        // worker.AddWaypoint(new Vector2Int(1, 5));
+        // worker.AddWaypoint(new Vector2Int(10, 6));
+        // worker.AddWaypoint(new Vector2Int(3, 2));
+        // worker.AddWaypoint(new Vector2Int(12, 14));
 
         // var b = new Vector3Int(1, 2, 0);
         // var o = Map.GetComponent<Grid>().CellToWorld(b);
@@ -55,6 +50,13 @@ public class GameManager : MonoBehaviour
         //     var w = Instantiate(WorkerPrefab, world, Quaternion.identity, Map.transform);
         //     w.GetComponentInChildren<SpriteRenderer>().color = Color.blue;
         // }
+
+        foreach (var coord in PathFinder.CalculateRoute(Map, new Vector2Int(1, 2), new Vector2Int(10, 6)))
+        {
+            var world = Map.GetComponent<Grid>().CellToWorld(new Vector3Int(coord.x, coord.y, 0));
+            var w = Instantiate(WorkerPrefab, world, Quaternion.identity, Map.transform);
+            w.GetComponentInChildren<SpriteRenderer>().color = Color.blue;
+        }
     }
 
     void Update()
