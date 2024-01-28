a0372c7f4974a2c7c7a8e4fd199300443fd17a99
Victor Erickson <vericks101@gmail.com>
2024-01-28 15:20:47 -0800

    Fixed issue where tooltip offset was not scaling with screen resolution. - Also WIP on hooking up tooltip with placement content.

--- Mountain/Assets/UI/Tooltip/TooltipUI.cs
+++ Mountain/Assets/UI/Tooltip/TooltipUI.cs
@@ -1,12 +1,25 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TooltipUI : MonoBehaviour
 {
     [SerializeField] private GameObject tooltipContentSection;
 
-    [SerializeField] private Vector2 tooltipOffset;
+    [SerializeField] private Vector2 tooltipOffsetFactor;
+    
+    [SerializeField] private TextMeshProUGUI tooltipPlacementNameText;
+    [SerializeField] private TextMeshProUGUI tooltipPlacementDescriptionText;
+    [SerializeField] private TextMeshProUGUI tooltipPlacementLostChanceText;
+    [SerializeField] private TextMeshProUGUI tooltipPlacementPassableText;
+    [SerializeField] private GameObject tooltipPlacementAbilityPrefab;
+    [SerializeField] private Transform tooltipPlacementAbilitiesSection;
+    [SerializeField] private GameObject tooltipPlacementOnVisitPrefab;
+    [SerializeField] private Transform tooltipPlacementOnVisitsSection;
 
     void Awake()
     {
@@ -16,12 +29,13 @@ public class TooltipUI : MonoBehaviour
 
     void ShowTooltipUI(Placement placement)
     {
-        Debug.Log(placement.Name);
         tooltipContentSection.transform.position = new Vector3(
-            Input.mousePosition.x + tooltipOffset.x, 
-            Input.mousePosition.y + tooltipOffset.y, 
+            Input.mousePosition.x + (tooltipOffsetFactor.x * Screen.width), 
+            Input.mousePosition.y + (tooltipOffsetFactor.y * Screen.height), 
             Input.mousePosition.z
         );
+
+        RefreshTooltipUIContent(placement);
 
         tooltipContentSection.SetActive(true);
     }
@@ -29,5 +43,13 @@ public class TooltipUI : MonoBehaviour
     void HideTooltipUI()
     {
         tooltipContentSection.SetActive(false);
+    }
+
+    void RefreshTooltipUIContent(Placement placement)
+    {
+        tooltipPlacementNameText.text = placement.Name;
+        tooltipPlacementDescriptionText.text = placement.FlavorText;
+        tooltipPlacementLostChanceText.text = String.Format("Chance to get lost: {0}%", (placement.LostChance * 100));
+        
     }
 }
