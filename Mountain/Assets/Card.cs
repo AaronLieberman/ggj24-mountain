5f8817ad832877e938b148b2fed42684b730a957
Aaron Lieberman <aaronlcode@gmail.com>
2024-01-28 11:59:04 -0800

    only one spawn per card now

--- Mountain/Assets/Card.cs
+++ Mountain/Assets/Card.cs
@@ -4,13 +4,6 @@ using System.Linq;
 using UnityEngine;
 
 [Serializable]
-public class TileSpawnDesc
-{
-    public Placement Tile;
-    public float Weight = 1;
-}
-
-[Serializable]
 public class CardDetails
 {
     public Sprite CardSprite;
@@ -18,7 +11,8 @@ public class CardDetails
 
 public class Card : MonoBehaviour
 {
-    public List<TileSpawnDesc> TileSpawn;
+    public Placement PlacementToSpawn;
+    public bool IsRevealed;
 
     public CardDetails CardDetails;
 
@@ -30,28 +24,9 @@ public class Card : MonoBehaviour
 
     public void SpawnOnTile(Tile tile)
     {
-        tile.SpawnPlacement(ChoosePlacementToSpawn());
-    }
-
-    Placement ChoosePlacementToSpawn()
-    {
-        if (!TileSpawn.Any())
-            return null;
-
-        var totalWeight = TileSpawn.Sum(a => a.Weight);
-        if (totalWeight <= 0)
-            return TileSpawn[_random.NextInt(TileSpawn.Count)].Tile;
-
-        var target = _random.NextFloat(totalWeight);
-
-        float weightSoFar = 0;
-        foreach (var spawn in TileSpawn)
+        if (PlacementToSpawn != null)
         {
-            weightSoFar += spawn.Weight;
-            if (target < weightSoFar)
-                return spawn.Tile;
+            tile.SpawnPlacement(PlacementToSpawn);
         }
-
-        return null;
     }
 }
