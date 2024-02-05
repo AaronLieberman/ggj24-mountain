bff3047aa23efe4f60138b90c07979768f780eba
Eric Carter <gitmeongithub@greenstorm.net>
2024-02-05 08:18:32 -0800

    made an action for removing tiles from the journey plan

---
+++ Mountain/Assets/Actions/RemoveTileFromJourneyPlan.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class RemoveTileFromJourneyPlan : PlacementAction
+{
+    public bool RemoveNextTile = false;
+    public bool RemoveRandomTile = false;
+    public bool RemoveAllTiles = false;
+    public bool RemoveLastTile = false;
+
+    public bool ShouldPutOnTopOfDeck = false;
+
+    public override void DoWork(Worker worker, Placement placement, Card card)
+    {
+        if (RemoveNextTile) RemoveTile(worker.WorkerPlans.First());
+
+        if (RemoveRandomTile)
+        {
+            int randomIndex = Random.Range(0, worker.WorkerPlans.Count);
+            RemoveTile(worker.WorkerPlans[randomIndex]);
+        }
+
+        if (RemoveAllTiles)
+        {
+            foreach(WorkerPlan pl in worker.WorkerPlans)
+            {
+                RemoveTile(pl);
+            }
+        }
+
+        if (RemoveLastTile) RemoveTile(worker.WorkerPlans.Last());
+
+    }
+
+    public void RemoveTile(WorkerPlan plan)
+    {
+        if (ShouldPutOnTopOfDeck) PutOnDeck(plan);
+        plan.Card = null;
+    }
+
+    protected void PutOnDeck(WorkerPlan cardToAdd)
+    {
+        Utilities.GetRootComponent<Deck>().AddNewCardToDeck(cardToAdd.Card.PlacementToSpawn, true);
+    }
+
+}
